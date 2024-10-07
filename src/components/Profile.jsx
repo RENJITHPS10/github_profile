50cc6e18c3a448186db6829c6641adcf2c80498d
RENJITHPS10 <renjithpstech@gmail.com>
2024-10-07 22:18:50 +0530

    changes added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,16 +9,16 @@ function Profile() {
             <div className="d-flex flex-column align-items-center mt-3 ms-sm-5">
                <div className='ms-sm-5 '>
                     <div className=" py-4   ">
-                        <img src="/src/assets/profile.jpg" className="rounded-circle" alt="" height="290px" width="290px" />
+                        <img src="/src/assets/profile.jpg" className="rounded-circle profile" alt="" height="290px" width="290px" />
                     </div>
                     <div >
                         <h6 className="text-white fs-4 m-0">RENJITH PS</h6>
-                        <h5 className="fw-light text-warning" >RENJITHPS10</h5>
+                        <h5 className="fw-light text-warning mb-3" >RENJITHPS10</h5>
                         <button className="btn btn-success border-1 border-secondary   w-100 fw-normal  p-0" style={{height:'2rem'}}>Edit profile</button>
     
                         <div className="d-flex">
                             <a href="" className="d-flex align-items-center mt-3 text-decoration-none">
-                            <FontAwesomeIcon icon={faUserGroup} className='text-warning'  />
+                            <FontAwesomeIcon icon={faUserGroup} className='text-warning fa-sm'  />
                                 <p className="mb-0 ms-2 text-warning" >
                                     <span className="text-white">6</span> followers
                                 </p>
@@ -32,13 +32,13 @@ function Profile() {
                         </div>
                         <a href="https://renjithps10.github.io/portfolio/" className="text-decoration-none ">
                             <p className="text-white d-flex align-items-center mt-3" style={{ fontSize: '14px' }}>
-                            <FontAwesomeIcon icon={faLink} className='text-warning me-2 ' />
+                            <FontAwesomeIcon icon={faLink} className='text-warning me-2 fa-sm  ' />
                                 https://renjithps10.github.io/portfolio/
                             </p>
                         </a>
                         <a href="https://www.linkedin.com/in/renjithps/" className="text-decoration-none ">
                             <p className="text-white d-flex align-items-center mt-3 " style={{ fontSize: '14px' }}>
-                            <FontAwesomeIcon icon={faLinkedin} className='text-warning me-2' />
+                            <FontAwesomeIcon icon={faLinkedin} className='text-warning me-2 fa-lg'  />
                                 in/renjithps
                             </p>
                         </a>
